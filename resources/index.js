283d3aff0c56238e4ba0f9cce39c3bdc37bc1e59
John Andrew Dixon <john.a.dixon@outlook.com>
2022-07-03 19:28:22 -0700

    Added something to prevent double events on touch.

--- resources/index.js
+++ resources/index.js
@@ -39,6 +39,10 @@ const calculateRating = () => {
 };
 
 const handleClick = event => {
+
+    if(event.type === 'touchstart') //If the event is triggered by a touch, this small branch prevents the code from being triggered again as a click.
+        event.preventDefault();
+
     const direction = event.target.getAttribute('class');
     const criterion = criteria[event.target.parentNode.id];
 
